53351296b1d98126cffb98d9cfae7d724b66ca68
mz-zarei <mzarei@uwaterloo.ca>
2022-01-31 10:08:13 -0500

    adding first file

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+
+// import the standard libary
+#include <iostream>
+
+// import cout and endl to global space from std namespace
+using std::cout;
+using std::endl;
+
+// define main fucntion which prints "Hello World!" into console
+int main() {
+  cout << "Hello World!" << endl; 
+  return 0;
+}
