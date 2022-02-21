ce79cec78051baba74823e1791bc9d4586796f6b
mz-zarei <mzarei@uwaterloo.ca>
2022-02-21 11:58:31 -0500

    add week1 challenge reverse queue/stack

--- Course2/Week1/StackVsQueue/README.md
+++ Course2/Week1/StackVsQueue/README.md
@@ -1,3 +1,5 @@
 # Stack versus Queue in C++
 
 A simple code to compare the behavior of Queue to Stack in C++.
+- Practice corresponding methods
+- Write function to reverse the elements order
---
+++ Course2/Week1/StackVsQueue/reverseQueueStack.cpp
@@ -0,0 +1,77 @@
+/**
+ * @brief Challenge Week1: Reversing Given Queue/Stack
+ * @file TestQueue.cpp
+ * @author Mohammad Zarei 
+ * Contact: <mzarei@uwaterloo.ca>
+ * 
+ */
+#include <iostream>
+#include <stack>
+#include <queue>
+
+std::stack<int> reverse_stack(std::stack<int> s) {
+  std::stack<int> reversed_s;
+  
+  while (!s.empty())
+    {
+    reversed_s.push(s.top());
+    s.pop();
+    }
+  return reversed_s;
+}
+
+std::queue<int> reverse_queue(std::queue<int> q) {
+  std::stack<int> temp_s;
+  while (!q.empty())
+    {
+    temp_s.push(q.front());
+    q.pop();
+    }
+  while (!temp_s.empty())
+    {
+    q.push(temp_s.top());
+    temp_s.pop();
+    }  
+  return q;
+}
+
+void print_stack(std::string name, std::stack<int> s) {
+  std::cout << "stack " << name << ": ";
+  while (!s.empty()) {
+    std::cout << s.top() << " ";
+    s.pop();
+  }
+  std::cout << std::endl;
+}
+
+void print_queue(std::string name, std::queue<int> q) {
+  std::cout << "queue " << name << ": ";
+  while (!q.empty()) {
+    std::cout << q.front() << " ";
+    q.pop();
+  }
+  std::cout << std::endl;
+}
+
+int main() {
+  std::stack<int> s, rs;
+  std::queue<int> q, rq;
+  
+  s.push(1); s.push(2); s.push(3); s.push(4); s.push(5);
+
+  print_stack("s",s);
+
+  rs = reverse_stack(s);
+
+  print_stack("reversed s",rs);
+  
+  q.push(1); q.push(2); q.push(3); q.push(4); q.push(5);
+  
+  print_queue("q",q);
+  
+  rq = reverse_queue(q);
+  
+  print_queue("reversed q",rq);
+
+  return 0;
+}
